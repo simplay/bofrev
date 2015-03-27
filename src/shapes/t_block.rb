b0edf60df22648438008d8fb11839eb58f9ec611
simplay <michael.single@students.unibe.ch>
2015-03-27 11:31:09 +0100

    Rotating all Shapes clock-wise

--- src/shapes/t_block.rb
+++ src/shapes/t_block.rb
@@ -7,9 +7,9 @@ class TBlock < Shape
   def position_states
     [
         [Point2f.new(-1,1), Point2f.new(0,1), Point2f.new(1,1), Point2f.new(0,0)],
-        [Point2f.new(1,1), Point2f.new(1,0), Point2f.new(1,-1), Point2f.new(0,0)],
+        [Point2f.new(-1,1), Point2f.new(-1,0), Point2f.new(-1,-1), Point2f.new(0,0)],
         [Point2f.new(-1,-1), Point2f.new(0,-1), Point2f.new(1,-1), Point2f.new(0,0)],
-        [Point2f.new(-1,1), Point2f.new(-1,0), Point2f.new(-1,-1), Point2f.new(0,0)]
+        [Point2f.new(1,1), Point2f.new(1,0), Point2f.new(1,-1), Point2f.new(0,0)]
     ]
   end
 end
