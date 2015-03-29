3ec0fedc352b0e89d41eab79b26f643aebe8f1a0
simplay <michael.single@students.unibe.ch>
2015-03-29 05:04:32 +0200

    Implemented proper SINGLETON pattern for nil_game_field
    initial work for reworking whole game_field state handling.

--- src/nil_game_field.rb
+++ src/nil_game_field.rb
@@ -1,8 +1,17 @@
-class NilGameField
+require_relative 'game_field'
+
+class NilGameField < GameField
+
+  def self.instance
+    if @instance.nil?
+      @instance = NilGameField.new
+    end
+    @instance
+  end
 
   def initialize(color = 'white', type = :field)
-    @color = color; @type = type
     @is_sentinel = true
+    super(color, type)
   end
 
 end
