e52b53ea0ce2274ffeff13c7af9b57b957267458
simplay <michael.single@students.unibe.ch>
2015-03-31 15:44:22 +0200

    Cleaned up code of CollisionChecker
    corrected some typos and tweaked docs.

--- src/tetris/collision_checker.rb
+++ src/tetris/collision_checker.rb
@@ -1,28 +1,25 @@
-require_relative '../settings'
 require_relative '../point2f'
 
-# states:
-#   bounded: cannot move any further sidewards (left, right)
-#   grounded: either floor hit or another already placed shape
-#   moveable: next update does not result in a collision
-
+# CollisionChecker Checks whether current shape movement was legal.
+# If issue was present, determines what kind of issue was present.
+# Collision state is stored in CollisionChecker#state.
+#
+# Detectable states:
+#   + bounded: cannot move any further sidewards (left, right).
+#   + grounded: either floor hit or another already placed shape.
+#   + moveable: next update does not result in a collision.
 class CollisionChecker
-
-  #include Settings
 
   attr_reader :state
 
-  # Check whether current movement was legal.
-  # If issue was present, determines what kind of issue was present.
-  #
   # @param shape [Shape] to check whether it collides with its map.
-  # @param opertaion [Symbol] what operation was performed on shape.
+  # @param operation [Symbol] what operation was performed on shape.
   #        is either equal to :rotate or :move
   # @param shift [Point2f] relative shift amount. Only defined for :move operations.
-  def initialize(shape, opertaion, shift = Point2f.new(0,0))
+  def initialize(shape, operation, shift = Point2f.new(0,0))
     @state = :moveable
 
-    if opertaion == :rotate
+    if operation == :rotate
       next_rot_hit_points = shape.next_rotation_position.map do |point|
         Point2f.new(point.x + shape.origin.x + 1, point.y + shape.origin.y + 1)
       end
@@ -33,7 +30,7 @@ class CollisionChecker
       end
       @state = :bounded if has_collision
 
-    elsif opertaion == :move
+    elsif operation == :move
       next_move_hit_points = shape.next_translated_shape(shift)
 
       hit_ground = next_move_hit_points.any? do |pos|
@@ -47,7 +44,7 @@ class CollisionChecker
         shape.apply_combo_check
       end
 
-    elsif opertaion == :move_sidewards
+    elsif operation == :move_sidewards
       next_shape_state_positions = shape.next_translated_shape(shift)
 
       # check whether one cell of the new shape location produces a collision.
