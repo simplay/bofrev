755257dd5bf12138a952dc79913d9699b3d0b631
simplay <michael.single@students.unibe.ch>
2015-04-20 18:52:47 +0200

    Fractal View Code cleanup

--- src/views/fractal_view.rb
+++ src/views/fractal_view.rb
@@ -5,6 +5,7 @@ class FractalView
   MUTE = true
   LAST_X_PIXEL = 800.0
   LAST_Y_PIXEL = 800.0
+  BITS_PER_COLOR_CHANNEL = 8
   MAX_ITER = 255
   def initialize
     build_gui_components
@@ -41,6 +42,7 @@ class FractalView
     s = (max_e - min_e) / (max_a - min_a)
     min_e + s*(x-min_a)
   end
+
   # Transforms pixel coordianates into zoomed fractal domain coordinates.
   # E.g. Mandelbrot set is defined within the domain [-2.5,1]x[-1,1].
   #
@@ -57,6 +59,7 @@ class FractalView
 
     x0 = transform_pixel_to_range(0.0, LAST_X_PIXEL, zoomed_x_min, zoomed_x_max, p_x)
     y0 = transform_pixel_to_range(0.0, LAST_Y_PIXEL, zoomed_y_min, zoomed_y_max, p_y)
+
     x = 0.0; y = 0.0;
     max_iter = MAX_ITER
     iter = 0
@@ -70,7 +73,7 @@ class FractalView
     end
 
     if iter < max_iter
-      bits = 8
+      bits = BITS_PER_COLOR_CHANNEL
       dig = 2**bits
       r = (dig-1) - iter%dig
       g = iter%dig
@@ -81,7 +84,6 @@ class FractalView
       r = (r+depth)%dig
       g = (g+depth)%dig
       b = (b+depth)%dig
-
       color = compute_color_string(r,g,b, bits)
 
       TkcRectangle.new(@canvas, p_x, p_y, p_x, p_y,
@@ -90,6 +92,7 @@ class FractalView
 
   end
 
+  # Compute a 3*bits TK fromatted color string.
   def compute_color_string(r,g,b, bits)
     r_s = prefix_zeros(bits, r)+r.to_s(2) 
     g_s = prefix_zeros(bits, g)+g.to_s(2) 
@@ -106,37 +109,4 @@ class FractalView
     seq.join
   end
 
-
-  def to_hsv(r, g, b)
-    max = [r,g,b].max
-    min = [r,g,b].min
-    delta = max - min
-    if max == min
-      h = 0.0
-    elsif max == r
-      h = 60.0*(0 + (g-b)/delta)
-    elsif max == g
-      h = 60.0*(2 + (b-r)/delta)
-    elsif max == b
-      h = 60.0*(4 + (r-g)/delta)
-    end
-    h = h + 360.0 if (h < 0.0)
-
-    s = (max == 0)? 0.0 : delta/max
-    v = max
-    [h,s,v]
-  end
-
-
-  # @param color [Integer] computed color value
-  # @return [String] TKTinter color 12 bit format. 
-  def to_12bit_color(color, bits)
-    prefix = ((0..((bits-color.to_s(2).length))).map do "0" end).join
-    color = "#{prefix}#{color.to_s(2)}"
-    color =  (color.split("").map do |char| (char == '0')? '0' : 'f' end).join 
-    color = "##{color}"
-    color = color[0..9] if color.length > 10
-    puts color unless MUTE
-    color
-  end
 end
