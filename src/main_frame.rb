a76e562fad90065bbf6a98127f948104cf4aa4ee
simplay <michael.single@students.unibe.ch>
2015-08-18 03:36:38 +0200

    Added borderlayout to mainframe

--- src/main_frame.rb
+++ src/main_frame.rb
@@ -1,8 +1,9 @@
 require 'my_canvas'
 require 'control_constants'
-
+require 'game_settings'
 require 'java'
 java_import 'javax.swing.JFrame'
+java_import 'java.awt.BorderLayout'
 
 class MainFrame < JFrame
 
@@ -15,13 +16,16 @@ class MainFrame < JFrame
   end
 
   def init_gui
+    setLayout BorderLayout.new
     @canvas = MyCanvas.new
     @canvas.game = @game
-    getContentPane.add(@canvas)
+    getContentPane.add(@canvas, BorderLayout::CENTER)
     setDefaultCloseOperation(JFrame::EXIT_ON_CLOSE)
-    setSize(700, 700)
+    offset = 7
+    setSize(GameSettings.max_width+offset, GameSettings.max_height+3)
     setLocationRelativeTo(nil)
     setVisible(true)
+    requestFocusInWindow()
   end
 
   def update_canvas
