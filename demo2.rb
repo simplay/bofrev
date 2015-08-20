a66e029162905b420fa1e904ec94d1764e995693
simplay <michael.single@students.unibe.ch>
2015-08-20 19:08:37 +0200

    Implemented working ruby music player demo
    using Jlayer - see http://introcs.cs.princeton.edu/java/faq/mp3/mp3.html

--- demo2.rb
+++ demo2.rb
@@ -1,36 +1,20 @@
 require 'java'
+require 'libs/jl1.0.1.jar'
+
 java_import "java.io.FileInputStream"
-java_import "java.io.InputStream"
-java_import "sun.audio.AudioStream"
-java_import "sun.audio.AudioDataStream"
-java_import "sun.audio.AudioPlayer"
-java_import "sun.audio.AudioData"
-java_import "javax.sound.sampled.AudioSystem"
-java_import "sun.audio.ContinuousAudioDataStream"
-java_import "java.io.BufferedInputStream"
+java_import "javazoom.jl.player.Player"
 
-file = "tetris.wav"
+file = "tetris_tone_loop.mp3"
 input_stream = FileInputStream.new("audio/#{file}")
-input_stream2 = FileInputStream.new("audio/#{file}")
-
-stream = BufferedInputStream.new(input_stream, 1024)
-ais = AudioSystem.getAudioInputStream( stream )
-format = ais.getFormat()
-
-# create correct audio buffer - currently buggy 
-# (byte format not correctly assinged)
-audio_stream = AudioStream.new(input_stream2)
-length = audio_stream.getLength()
-buffer = Java::byte[length].new;
-ais.read(buffer, 0, length);
-
-ad = AudioData.new(buffer)
-ads = AudioDataStream.new(ad)
-
-# for looping sounds
-cont_audio_stream = ContinuousAudioDataStream.new(ad)
-
-# for triggerd sounds
-audio_stream = AudioDataStream.new(ad);
-
-AudioPlayer.player.start(audio_stream)
+player = Player.new(input_stream)
+player.play(30)
+@thread = Thread.new do
+  loop do
+    puts "is complete: #{player.isComplete} p: #{player.getPosition}"
+      input_stream = FileInputStream.new("audio/#{file}")
+      player = Player.new(input_stream)
+      player.play(30)
+  end
+end
+puts "foo"
+@thread.join
