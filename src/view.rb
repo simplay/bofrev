e90f1bc5d40d03c99576493fa4ea4bc39e2e6b73
simplay <michael.single@students.unibe.ch>
2015-08-21 06:12:46 +0200

    Detecting mouse events

--- src/view.rb
+++ src/view.rb
@@ -5,6 +5,7 @@ require 'main_frame'
 
 require 'java'
 java_import 'java.awt.event.KeyListener'
+java_import 'java.awt.event.MouseListener'
 
 class View < Observer
 
@@ -12,17 +13,27 @@ class View < Observer
     game.subscribe(self)
     @game = game
     @main_frame = MainFrame.new(game)
-    attach_key_listener
+    attach_listeners
     clicked_onto_start
   end
 
-  def attach_key_listener
+  def attach_listeners
     @main_frame.add_key_listener KeyListener.impl { |name, event|
       # key_debugger(name, event)
       value_pressed_key = event.getKeyChar.chr
       event_identifier = "#{name}_#{value_pressed_key}"
       handle_pressed_key(event_identifier) if allowed_event?(event_identifier)
     }
+
+    @main_frame.addMouseListener do |event|
+      x = event.getX
+      y = event.getY
+      button = event.getButton
+      type = event.paramString.split(",").first
+      identifier = "#{type.downcase}_#{button}"
+      handle_mouse_events(x, y, identifier)
+      puts "mouse event: #{type}, b=#{button}, (#{x},#{y})"
+    end
   end
 
   def allowed_event?(identifier)
@@ -39,6 +50,12 @@ class View < Observer
   def handle_pressed_key(type)
     puts "handling event: #{type}"
     @game.perform_loop_step(Event.new(type)) unless @game.finished?
+  end
+
+  def handle_mouse_events(x,y, type)
+    puts "handling event: #{type} at (#{x}, #{y})"
+    message = Event.new(type, Point2f.new(x,y))
+    #@game.perform_loop_step(message)
   end
 
   def handle_event
