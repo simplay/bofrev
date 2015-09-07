3881739547a8447a67a93e89667403e3d1516494
simplay <michael.single@students.unibe.ch>
2015-09-08 00:05:46 +0200

    Introduced audio playback listener and callbacks
    will be used for remembering last played after having pressed the
    suspended button.

--- src/java_music_player.rb
+++ src/java_music_player.rb
@@ -2,12 +2,26 @@ require 'java'
 require_relative '../lib/jl1.0.1.jar'
 java_import "java.io.FileInputStream"
 java_import "javazoom.jl.player.advanced.AdvancedPlayer"
+java_import "javazoom.jl.player.advanced.PlaybackListener"
 
 class JavaMusicPlayer
+
+  # Java::JavazoomJlPlayerAdvanced::PlaybackListener
+  class MyPlaybackListener < Java::JavazoomJlPlayerAdvanced::PlaybackListener
+
+    def playbackFinished(event)
+      puts "finished"
+    end
+
+    def playbackStarted(event)
+      puts "started"
+    end
+  end
 
   def initialize(file)
     @file = file
     @is_runnable = true
+    @pbl = MyPlaybackListener.new
   end
 
   def pause
@@ -43,6 +57,7 @@ class JavaMusicPlayer
   def run_sample(start_frame=0, end_frame=1000000000)
     input_stream = FileInputStream.new(@file)
     @jmp = Java::JavazoomJlPlayerAdvanced::AdvancedPlayer.new(input_stream)
+    @jmp.setPlayBackListener(@pbl)
     @jmp.play(start_frame, end_frame)
     sleep 0.5
   end
