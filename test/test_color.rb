a3846cdaf271310e11245f1826cdafa48314216e
simplay <michael.single@students.unibe.ch>
2015-09-11 17:46:55 +0200

    Wrote initial color minitest

--- test/test_color.rb
+++ test/test_color.rb
@@ -1,7 +1,16 @@
 require "minitest/autorun"
+require 'color'
 
 class TestColor < Minitest::Test
-  def test_foo
-    assert_equal(true, true)
+
+  def setup
+    @random_24_bit_rgb_value = "#"+(3.times.map do rand(256).to_s(16) end).join
+    @random_color = Color.new(@random_24_bit_rgb_value)
   end
+
+  def test_to_rgb
+    assert_equal(@random_color.to_rgb, @random_24_bit_rgb_value)
+  end
+
+
 end
