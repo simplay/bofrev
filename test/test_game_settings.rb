590abd0fb4575b97ac80b724153237fb1e5876d2
simplay <michael.single@students.unibe.ch>
2015-09-14 03:20:47 +0200

    Testing GameSettings meta_data, render_attributes getters

--- test/test_game_settings.rb
+++ test/test_game_settings.rb
@@ -1,5 +1,5 @@
 require "minitest/autorun"
-
+require 'pry'
 class TestGameSettings < Minitest::Test
 
   # Runs bofore every test, before teardown.
@@ -98,4 +98,140 @@ class TestGameSettings < Minitest::Test
     assert_equal(GameSettings.run_music?, false)
     assert_equal(GameSettings.run_game_thread?, false)
   end
+
+  def test_game_meta_data_is_correctly_build_for_selected_game
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data, rand_game_settings.derive_game_model)
+  end
+
+  def test_canvas_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.canvas, GameSettings.canvas)
+  end
+
+  def test_selected_gui_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.gui_type_as_sym, GameSettings.selected_gui)
+  end
+
+  def test_gui_to_build_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.gui_type, GameSettings.gui_to_build)
+  end
+
+  def test_game_map_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.game_map, GameSettings.game_map)
+  end
+
+  def test_theme_list_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.theme_list, GameSettings.theme_list)
+  end
+
+  def test_sound_effect_list_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.sound_effect_list,
+                 GameSettings.sound_effect_list)
+  end
+
+  def test_achievement_system_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.achievement_system,
+                 GameSettings.achievement_system)
+  end
+
+  def test_achievement_system_sym_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.achievement_system_sym,
+                 GameSettings.achievement_system_sym)
+  end
+
+  def test_render_attributes_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.render_attributes,
+                 GameSettings.render_attributes)
+  end
+
+  def test_allowed_controls_is_from_game_meta_data
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.allowed_controls,
+                 GameSettings.allowed_controls)
+  end
+
+  def test_cell_size_is_from_render_attributes
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.render_attributes[:cell_size],
+                 GameSettings.cell_size)
+  end
+
+  def test_width_pixels_is_from_render_attributes
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.render_attributes[:width_pixels],
+                 GameSettings.width_pixels)
+  end
+
+  def test_height_pixels_is_from_render_attributes
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.render_attributes[:height_pixels],
+                 GameSettings.height_pixels)
+  end
+
+  def test_max_height_is_from_render_attributes
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.render_attributes[:max_height],
+                 GameSettings.max_height)
+  end
+
+  def test_max_width_is_from_render_attributes
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.render_attributes[:max_width],
+                 GameSettings.max_width)
+  end
+
+  def test_tics_per_second_is_from_render_attributes
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.render_attributes[:tics_per_second],
+                 GameSettings.tics_per_second)
+  end
+
+  def test_show_grid_is_from_render_attributes
+    rand_game_idx = rand(1..7) # any available game
+    rand_game_settings = GameSettings.build_from({:game => rand_game_idx})
+    assert_equal(rand_game_settings.game_meta_data.render_attributes[:show_grid] || true,
+                 GameSettings.show_grid?)
+  end
+
+  def test_show_grid_nil_case_do_show
+    rand_game_settings = GameSettings.build_from({:game => 6})
+    assert_equal(GameSettings.show_grid?, true)
+  end
+
+  def test_show_grid_true_case_do_show
+    rand_game_settings = GameSettings.build_from({:game => 1})
+    assert_equal(GameSettings.show_grid?, true)
+  end
+
+  def test_show_grid_false_case_dont_show
+    rand_game_settings = GameSettings.build_from({:game => 7})
+    assert_equal(GameSettings.show_grid?, false)
+  end
+
 end
