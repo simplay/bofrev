e956d1f37fc278393d986954eeeebbfd4bdc59ae
simplay <michael.single@students.unibe.ch>
2015-09-17 11:55:24 +0200

    Added missing Layer tests
    but skipping some of them

--- test/test_layer.rb
+++ test/test_layer.rb
@@ -14,4 +14,23 @@ class TestLayer < Minitest::Test
     assert_equal(layer.drawables.count, 2)
   end
 
+  def test_drawables
+    layer = Layer.new
+    d1 = ANewDrawableL.new
+    d2 = ANewDrawableL.new
+    d3 = ANewDrawableL.new
+    layer.append(d1)
+    layer.append(d2)
+    layer.append(d3)
+    assert_equal(layer.drawables, [d1,d2,d3])
+  end
+
+  def test_update_drawables
+    skip
+  end
+
+  def test_draw_drawables_onto
+    skip
+  end
+
 end
