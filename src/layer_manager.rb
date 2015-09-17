6f9f44272ff219d6cc0e167b83c6699d466adb4e
simplay <michael.single@students.unibe.ch>
2015-09-17 02:32:18 +0200

    Extended docs of LayerManager

--- src/layer_manager.rb
+++ src/layer_manager.rb
@@ -1,16 +1,22 @@
 require 'layer'
 
-# foreground: moving player shape
-# updated by player keyboard interactions.
-# center: bot shapes /obstacles (such as plotforms) a player can interact with.
-# updates by timer or by global.
-# background: background animations no interactions possible.
-# updated global ticker.
+# LayerManager is responsible for managing different Layer instances that have some
+# role associated with. Currently, the following Layer roles are defined:
+#   foreground layers: moving player shape
+#     updated by player keyboard interactions.
+#   center: bot shapes /obstacles (such as plotforms) a player can interact with.
+#     updates by timer or by global.
+#   background: background animations no interactions possible.
+#     updated global ticker.
 class LayerManager
 
+  # Define a Hash called @layers containing different types of layers.
+  #
+  # @hint: The hash key order in @layers determines the order of drawing:
+  #   first, the background layer will be drawn,
+  #   then on top of that, the center layer.
+  #   lastly, the foreground layer will be drawn.
   def initialize
-    # hash key order determines the order of drawing:
-    # first, the background layer will be drawn, then on top of that, the center layer. lastly, the foreground layer will be drawn.
     @layers = {
       :background => Layer.new,
       :center => Layer.new,
@@ -28,23 +34,38 @@ class LayerManager
     end
   end
 
+  # Update all Drawable instances that are appended to a target layer
+  # selected by a given layer type from @layers.
+  #
+  # @param layer_type [Symbol] layer key of target layer.
   def update_layer(layer_type)
     layer_with(layer_type).update_drawables
   end
 
+  # Draw all Drawables contained in the layers of this LayerManager.
+  #
+  # @param graphics [Java::Graphics] target Awt graphics object
   def draw_drawables_onto(graphics)
     @layers.values.each do |layer|
       layer.draw_drawables_onto(graphics)
     end
   end
 
-
+  # Draw all drawables that live in a target layer with a certain layer type.
+  #
+  # @param graphics [Java::Graphics] target Awt graphics object
+  # @param layer_type [Symbol] layer key of target layer.
   def draw_drawables_onto_for(graphics, layer_type)
     layer_with(layer_type).draw_drawables_onto(graphics)
   end
 
   protected
 
+  # Retrieve the target layer with a certain layer type.
+  # @example layer_with(:center) gives the Layer instance
+  #   that belongs to the Hash key :center in @layer.
+  # @param layer_type [Symbol] key in @layers identifying the target layer.
+  # @return [Layer] that belongs to the given layer_type argument.
   def layer_with(layer_type)
     @layers[layer_type]
   end
--- test/test_layer_manager.rb
+++ test/test_layer_manager.rb
@@ -2,11 +2,41 @@ require "test_helper"
 
 class TestLayerManager < Minitest::Test
 
+  # allow to fetch puts outputs
+  def fetch_stdout(&block)
+    begin
+      old_stdout = $stdout
+      $stdout = StringIO.new('','w')
+      yield block
+      $stdout.string
+    ensure
+      $stdout = old_stdout
+    end
+  end
+
+  class ADrawable
+  end
+
   def setup
     LayerManager.class_eval do
       def layers
         @layers
       end
+    end
+
+    Layer.class_eval do
+      def drawables
+        @drawables
+      end
+
+      def update_drawables
+        puts "update #{object_id}"
+      end
+
+      def draw_drawables_onto(g)
+        puts "draw #{object_id}"
+      end
+
     end
   end
 
@@ -17,6 +47,43 @@ class TestLayerManager < Minitest::Test
     assert(lm.layers.values[0].is_a?(Layer))
     assert(lm.layers.values[1].is_a?(Layer))
     assert(lm.layers.values[2].is_a?(Layer))
+    assert_equal(lm.layers.values[0].drawables, [])
+    assert_equal(lm.layers.values[1].drawables, [])
+    assert_equal(lm.layers.values[2].drawables, [])
+  end
+
+  def test_append_to
+    lm = LayerManager.new
+    a_drawable1 = ADrawable.new
+    a_drawable2 = ADrawable.new
+    a_drawable3 = ADrawable.new
+    lm.append_to([a_drawable1, a_drawable2], :background)
+    lm.append_to([a_drawable3], :foreground)
+    assert(lm.layers.values[0].drawables.include?(a_drawable1))
+    assert(lm.layers.values[0].drawables.include?(a_drawable2))
+    assert_equal(lm.layers.values[0].drawables.count, 2)
+    assert(lm.layers.values[2].drawables.include?(a_drawable3))
+    assert_equal(lm.layers.values[2].drawables.count, 1)
+  end
+
+  def test_update_layer
+    lm = LayerManager.new
+    out = fetch_stdout{lm.update_layer(:center)}
+    assert(out.include?("update " + lm.layers.values[1].object_id.to_s))
+  end
+
+  def test_draw_drawables_onto
+    lm = LayerManager.new
+    out = fetch_stdout {lm.draw_drawables_onto(nil)}
+    assert_equal(out.include?("draw " + lm.layers.values[0].object_id.to_s), true)
+    assert_equal(out.include?("draw " + lm.layers.values[1].object_id.to_s), true)
+    assert_equal(out.include?("draw " + lm.layers.values[2].object_id.to_s), true)
+  end
+
+  def test_draw_drawables_onto_for
+    lm = LayerManager.new
+    out = fetch_stdout {lm.draw_drawables_onto_for(nil, :center)}
+    assert_equal(out.include?("draw " + lm.layers.values[1].object_id.to_s), true)
   end
 
 end
